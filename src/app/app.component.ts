60d806a9766d3cbf8e3b237c17cfc2095cc6d3df
entropia.studio <info@entropia.studio>
2018-09-28 13:06:34 +0200

    Resize map according to screen resolution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Nightlife Coordination Life';  
   lat: number = 51.678418;
   lng: number = 7.809007;
+  actualHeight: number;
 
+  constructor(){
+    // Actual space available in navigator    
+    this.actualHeight = window.innerHeight - 106;    
+  }
+
+  onResize($event){
+    this.actualHeight = window.innerHeight - 106;
+  }
+
+  ngOnInit(){    
+  }
 }
