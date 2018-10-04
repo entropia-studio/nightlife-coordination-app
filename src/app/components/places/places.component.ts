ff98210c81edb13d58e361b580f6f8bdf62e5032
entropia.studio <info@entropia.studio>
2018-10-04 13:30:58 +0200

    Fix bug with markers, now showed properly on MapReady. The problem was about the Angular Zone (ngZone) anf Google Maps

--- src/app/components/places/places.component.ts
+++ src/app/components/places/places.component.ts
@@ -1,113 +1,71 @@
-import { Component, OnInit, ViewChild, AfterContentInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone} from '@angular/core';
 import { Coordinates } from '../../interfaces/coordinates';
 import {} from '@types/googlemaps';
 import { Place } from '../../interfaces/place';
+
+
 
 @Component({
   selector: 'app-places',
   templateUrl: './places.component.html',
   styleUrls: ['./places.component.css']
 })
-export class PlacesComponent implements OnInit, AfterContentInit {
+export class PlacesComponent {
 
-  places: Place[] = [];
-  map: any;
-  //@ViewChild('agm-map', {read: ElementRef}) map: ElementRef;
-
+  places: Place[] = [];      
   coordinates: Coordinates;
+  currentHeight: number;
+  navsHeight: number  = 106;
   
   constructor(private ngZone: NgZone) {
     this.coordinates = {
       'lat'  : 39.5231711,
       'lng' : -0.4186977,
       'mapZoom' : 15
-    }
-    
+    }    
     // Current available space in navigator    
     this.currentHeight = window.innerHeight - this.navsHeight; 
-  }
-
-  currentHeight: number;
-  navsHeight: number  = 106;
-
-  ngOnInit() {
-  }
+  }  
 
   coordinatesChangedHandler = (coordinates: Coordinates) => {
     this.coordinates = coordinates;    
   }
 
-  ngAfterContentInit(){
-    //console.log('map',this.map.nativeElement)
-  }
 
-  mapReady(map: any){
-    this.map = map;
-    let places = new google.maps.places.PlacesService(map);
-    
-    //console.log('places',map)
-    
-    
-    let place = {'lat': this.coordinates.lat,'lng': this.coordinates.lng};
+  mapReady(map: any){    
+    let places = new google.maps.places.PlacesService(map);    
+    let place = {'lat': this.coordinates.lat,'lng': this.coordinates.lng}; 
     
     places.nearbySearch({
       location: place,
       radius: 500,
-      type: 'bar'
-    }, this.callback.bind(this))      
-    
-    
+      type: 'bar',      
+    }, this.callback.bind(this))          
   }
 
   callback(results, status) {
-    
-    
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
+    console.log(results)
+    // If it is not present the markers won't appear until fire an event on screen
+    // It's perentory to work inside Angular zona to detect changes
+    this.ngZone.run(() => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
       
-      for (var i = 0; i < results.length; i++) {
-        console.log(results[i])
-        this.places.push({
-          'name'    : results[i].name,
-          'image'   : results[i].icon,
-          'address' : results[i].vicinity,
-          'rating'  : results[i].rating,
-          'lat'     : results[i].geometry.location.lat(),
-          'lng'     : results[i].geometry.location.lng(),
-        })
-         
-        //this.createMarker(results[i]);
-
+        for (var i = 0; i < results.length; i++) {              
+          let urlImage = results[i].photos ? results[i].photos[0].getUrl({maxWidth: 300}) : results[i].icon;
+          if (results[i].photos){
+            console.log(results[i].photos[0].getUrl({maxWidth: 640}))
+          }
+          
+          this.places.push({
+            'name'    : results[i].name,
+            'image'   : urlImage,
+            'address' : results[i].vicinity,
+            'rating'  : Number(results[i].rating),
+            'lat'     : Number(results[i].geometry.location.lat()),
+            'lng'     : Number(results[i].geometry.location.lng()),
+          })                   
+        }              
       }
-      //this.places = placesArr;
-      console.log(this.places)
-    }
+    })        
   }
-
-  createMarker(place) {
-    console.log('place: ',this.places)
-    /*
-    this.places.push({
-      'name'    : place.name,
-      'image'   : place.icon,
-      'address' : place.vicinity,
-      'rating'  : place.rating
-    })
-    */
-    /*
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-      map: this.map,
-      position: place.geometry.location
-    });
-    /*
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent(place.name);
-      infowindow.open(map, this);
-    });
-    */
-  }
-
-
-
-
 }
