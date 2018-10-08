e2350ce8987880cffbbc858b9af5d75a15b81b08
entropia.studio <info@entropia.studio>
2018-10-08 15:42:37 +0200

    Add environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:  'AIzaSyD3KTjS0FttdaGNLW3zT_CdTKlOKmmhvwY'
 };
 
 /*
