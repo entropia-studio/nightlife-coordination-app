8c47d9c2043830b26f53adf84f10b9675364fcab
entropia.studio <info@entropia.studio>
2018-10-22 13:31:44 +0200

    Add environment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-  apiKey:  'AIzaSyD3KTjS0FttdaGNLW3zT_CdTKlOKmmhvwY'
+  apiKey:  'AIzaSyD3KTjS0FttdaGNLW3zT_CdTKlOKmmhvwY',
+  firebase: {
+    apiKey: "AIzaSyCR52OlgJH6HSk8KZ30y_l88FXv9-Z3z3w",
+    authDomain: "nightlife-coordi-1538037506623.firebaseapp.com",
+    databaseURL: "https://nightlife-coordi-1538037506623.firebaseio.com",
+    projectId: "nightlife-coordi-1538037506623",
+    storageBucket: "nightlife-coordi-1538037506623.appspot.com",
+    messagingSenderId: "585156913174"
+  }
 };
 
 /*
