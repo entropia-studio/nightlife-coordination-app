2d686fc0273adbacabc74312cc38b0153d744f58
entropia.studio <info@entropia.studio>
2018-10-23 12:36:51 +0200

    Remove marker-click directive

--- src/app/directives/marker-click.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MarkerClickDirective } from './marker-click.directive';
-
-describe('MarkerClickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MarkerClickDirective();
-    expect(directive).toBeTruthy();
-  });
-});
